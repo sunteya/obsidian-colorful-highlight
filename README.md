e1517fd62f6364d63364d7c7736fb10520c42f81
sunteya <sunteya@gmail.com>
2023-01-22 20:21:03 +0800

    simple implement

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Obsidian Sample Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+Auto colorful highlight block plugin for Obsidia.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+**Note:** You need to compile and install this plug-in yourself!
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+<table>
+  <tr>
+    <td><img src="./images/editing.png"></td>
+    <td><img src="./images/reading.png"></td>
+  </tr>
+</table>
 
 This sample plugin demonstrates some of the basic functionality the plugin API can do.
 - Changes the default font color to red using `styles.css`.
--- main.ts
+++ main.ts
@@ -1,137 +1,73 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian'
+import { syntaxTree } from "@codemirror/language"
+import {
+  Extension,
+  RangeSetBuilder,
+  StateField,
+  Transaction,
+} from "@codemirror/state"
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+} from "@codemirror/view"
 
-// Remember to rename these classes and interfaces!
+import { Md5 as MD5 } from 'ts-md5';
 
-interface MyPluginSettings {
-	mySetting: string;
+function colorfulStyle(text: string): { [x in string]: string } {
+	const sum = MD5.hashStr(text, true).reduce((a, b) => a + b, 0)
+	const deg = sum % 3600 / 10
+	return { filter: `hue-rotate(${deg}deg)` }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const ColorfulHighlightField = StateField.define<DecorationSet>({
+  create(state): DecorationSet {
+    return Decoration.none
+  },
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  update(oldState: DecorationSet, transaction: Transaction): DecorationSet {
+    const builder = new RangeSetBuilder<Decoration>();
+    syntaxTree(transaction.state).iterate({
+      enter(node) {
+        if (node.name.includes("highlight")) {
+					const from = node.from
+					const to = node.to
+					const text = transaction.state.doc.sliceString(from, to)
 
+          builder.add(
+            from,
+            to,
+            Decoration.mark({
+							class: "cm-highlight",
+							attributes: {
+								style: Object.entries(colorfulStyle(text)).map(([k, v]) => `${k}: ${v}`).join(';')
+							}
+            })
+          );
+        }
+      },
+    })
+
+    return builder.finish()
+  },
+
+  provide(field: StateField<DecorationSet>): Extension {
+    return EditorView.decorations.from(field)
+  },
+})
+
+export default class ColorfulHighlightPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.registerMarkdownPostProcessor((element, context) => {
+      const marks = element.querySelectorAll("mark");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+      for (let index = 0; index < marks.length; index++) {
+        const mark = marks.item(index)
+        const text = mark.innerText.trim()
+				Object.assign(mark.style, colorfulStyle(text))
+      }
+    })
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		this.registerEditorExtension(ColorfulHighlightField)
 	}
 }
